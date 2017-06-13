84e395e9cac6f6a61a45a7833b4f3452a902a2fc
Thomas Mailund <mailund@birc.au.dk>
2017-06-13 22:51:53 +0200

    Specify complexity of function.

--- stralg/stralg.h
+++ stralg/stralg.h
@@ -24,6 +24,7 @@ void build_border_array(const char * str, unsigned long n, unsigned long * ba);
 void build_z_array(const char * str, unsigned long n, unsigned long * Z);
 
 /* Builds the Z array from a border array.
+ * This function currently runs in worst-case quadratic time.
  * The Z array must be allocated before calling
  * the function.
  */
