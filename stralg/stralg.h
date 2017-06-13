7d2f11322857d8cc1d2ddf4107abf1f0083cecdf
Thomas Mailund <mailund@birc.au.dk>
2017-06-13 22:59:16 +0200

    Reformatted the documentation strings.
    I haven't updated the documentation for the function that computes Z from the border array since I am not yet sure that is something I can actually do in linear time.

--- stralg/stralg.h
+++ stralg/stralg.h
@@ -1,25 +1,41 @@
 #ifndef STRALG_H
 #define STRALG_H
 
-/* Count how many characters match in two 0 terminated strings.
+/**
+ Count how many characters match in two 0 terminated strings.
+
+ @param s1 A '\0' terminated string.
+ @param s2 A '\0' terminated string.
+ @return The number of characters that s1 and s2 match.
  */
 unsigned long match(const char * s1, const char * s2);
 
-/* Build the border array for string str, of length n,
- * and store it in ba. The border array ba has the 
- * property that ba[i] is the length of the longest
- * border of str[1:i].
- * The array must be allocated before
- * we compute the border array.
+/**
+ Build the border array for string str, of length n,
+ and store it in ba. The border array ba has the
+ property that ba[i] is the length of the longest
+ border of str[1:i].
+ 
+ The array must be allocated before we compute the border array.
+
+ @param str A string of length n.
+ @param n The length of str
+ @param ba A pre-allocated array to put the border information in.
  */
 void build_border_array(const char * str, unsigned long n, unsigned long * ba);
 
-/* The fundamental preprocessing from Gusfield 1.4,
- * computing the Z array. The Z array has the property
- * that Z[i] is the length of the longest substring
- * of str starting at position i that is also a prefix
- * of str. The Z array must be allocated before calling
- * the function.
+/**
+ The fundamental preprocessing from Gusfield 1.4,
+ computing the Z array. The Z array has the property
+ that Z[i] is the length of the longest substring
+ of str starting at position i that is also a prefix
+ of str.
+ 
+ The Z array must be allocated before calling the function.
+ 
+ @param str A string of length n.
+ @param n The length of str
+ @param Z A pre-allocated array to put the Z information in.
  */
 void build_z_array(const char * str, unsigned long n, unsigned long * Z);
 
