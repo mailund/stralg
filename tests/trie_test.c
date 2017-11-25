bdc686306b46d0b5019d68e18dd86acbd7abad4e
Thomas Mailund <mailund@birc.au.dk>
2017-11-25 18:09:56 +0100

    Added failure link tests to trie_test.
    Looks like the failure links are the *shortest* suffixes in the trie while they should be the *longest*.

--- tests/trie_test.c
+++ tests/trie_test.c
@@ -1,7 +1,7 @@
 #include <trie.h>
 #include <stdlib.h>
 #include <assert.h>
-
+#include <string.h>
 #include <stdio.h>
 
 static void reverse_string(char *begin, char *end)
@@ -40,12 +40,12 @@ int main(int argc, char * argv[])
     
     char buffer[10];
     extract_label(t, buffer);
-    printf("'%s'\n", buffer);
+    assert(strcmp(buffer, "aba") == 0);
     
     t = get_trie_node(trie, "ab");
     assert(t != 0);
     extract_label(t, buffer);
-    printf("'%s'\n", buffer);
+    assert(strcmp(buffer, "ab") == 0);
     
     assert(!string_in_trie(trie, "a"));
     assert(!string_in_trie(trie, "ab"));
@@ -58,6 +58,46 @@ int main(int argc, char * argv[])
     assert(!string_in_trie(trie, "babc"));
     
     compute_failure_links(trie);
+    struct trie *a = get_trie_node(trie, "a");
+    struct trie *a_failure = a->failure_link;
+    assert(is_trie_root(a_failure));
+    
+    struct trie *ab = get_trie_node(trie, "ab");
+    struct trie *ab_failure = ab->failure_link;
+    extract_label(ab_failure, buffer);
+    assert(strcmp(buffer, "b") == 0);
+    
+    struct trie *aba = get_trie_node(trie, "aba");
+    struct trie *aba_failure = aba->failure_link;
+    extract_label(aba_failure, buffer);
+    //assert(strcmp(buffer, "ba") == 0); fixme
+    printf("aba, failure should be ba but is %s\n", buffer);
+    
+    struct trie *abab = get_trie_node(trie, "abab");
+    struct trie *abab_failure = abab->failure_link;
+    extract_label(abab_failure, buffer);
+    //assert(strcmp(buffer, "bab") == 0); fixme
+    printf("abab, failure should be bab but is %s\n", buffer);
+    
+    struct trie *ababc = get_trie_node(trie, "ababc");
+    struct trie *ababc_failure = ababc->failure_link;
+    assert(is_trie_root(ababc_failure));
+    
+    
+    struct trie *b = get_trie_node(trie, "b");
+    struct trie *b_failure = b->failure_link;
+    assert(is_trie_root(b_failure));
+    
+    struct trie *ba = get_trie_node(trie, "ba");
+    struct trie *ba_failure = ba->failure_link;
+    extract_label(ba_failure, buffer);
+    assert(strcmp(buffer, "a") == 0);
+
+    struct trie *bab = get_trie_node(trie, "bab");
+    struct trie *bab_failure = bab->failure_link;
+    extract_label(bab_failure, buffer);
+    printf("bab, failure should be ab but is %s\n", buffer);
+    //fixme: assert(strcmp(buffer, "ab") == 0);
     
     delete_trie(trie);
     
