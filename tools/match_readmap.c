a5dd0f5c1446020811877be12b667e98dc9fe13a
Thomas Mailund <mailund@birc.au.dk>
2017-11-28 13:24:51 +0100

    exact pattern match readmapper

--- tools/match_readmap.c
+++ tools/match_readmap.c
@@ -1,8 +1,8 @@
 
+
 /*
- Readmapper based on explicitly generating approximate matches and
- using the Aho-Corasick algorithm for matching.
-*/
+ Readmapper based on exact pattern matching algorithms.
+ */
 
 #include <stralg.h>
 #include <stdlib.h>
@@ -10,10 +10,16 @@
 #include <stdio.h>
 #include <getopt.h>
 
+typedef void (*exact_match_func)(const char *text, size_t n,
+                                 const char *pattern, size_t m,
+                                 match_callback_func callback,
+                                 void *callback_data);
+
 struct search_info {
     int edit_dist;
     struct fasta_records *records;
     FILE *sam_file;
+    exact_match_func match_func;
 };
 
 static struct search_info *empty_search_info()
@@ -22,6 +28,7 @@ static struct search_info *empty_search_info()
         (struct search_info*)malloc(sizeof(struct search_info));
     info->edit_dist = 0;
     info->records = empty_fasta_records();
+    info->match_func = 0;
     return info;
 }
 
@@ -36,11 +43,9 @@ struct read_search_info {
     const char *read_name;
     const char *read;
     const char *quality;
-    FILE *sam_file;
-    
-    struct string_vector *patterns;
-    struct string_vector *cigars;
+    const char *cigar;
     struct trie *patterns_trie;
+    struct search_info *search_info;
 };
 
 static struct read_search_info *empty_read_search_info()
@@ -52,49 +57,56 @@ static struct read_search_info *empty_read_search_info()
     info->read_name = 0;
     info->read = 0;
     info->quality = 0;
-    
-    info->patterns = empty_string_vector(256); // arbitrary start size...
-    info->cigars = empty_string_vector(256); // arbitrary start size...
+    info->cigar = 0;
     info->patterns_trie = empty_trie();
-    
+    info->search_info = 0;
+
     return info;
 }
 
 static void delete_read_search_info(struct read_search_info *info)
 {
-    delete_string_vector(info->patterns);
-    delete_string_vector(info->cigars);
     delete_trie(info->patterns_trie);
     free(info);
 }
 
-static void build_trie_callback(const char *pattern, const char *cigar, void * data)
+static void match_callback(size_t index, void * data)
+{
+    struct read_search_info *info = (struct read_search_info*)data;
+    sam_line(info->search_info->sam_file,
+             info->read_name,
+             info->ref_name,
+             index,
+             info->cigar,
+             info->read,
+             info->quality);
+}
+
+static void pattern_callback(const char *pattern, const char *cigar, void * data)
 {
     struct read_search_info *info = (struct read_search_info*)data;
     
-    // patterns generated when we explore the neighbourhood of a read are not unique
-    // so we need to check if we have seen it before
+    // we use the trie to make sure we do not search for the same pattern
+    // more than once
     if (string_in_trie(info->patterns_trie, pattern))
         return; // nothing to see here, move along.
     
     // NB: the order is important here -- info->patterns->used will be updated
     // when we add the pattern to the vector, so we insert in the trie first.
-    add_string_to_trie(info->patterns_trie, pattern, info->patterns->used);
-    add_string_copy(info->patterns, pattern);
-    add_string_copy(info->cigars, cigar);
+    add_string_to_trie(info->patterns_trie, pattern, 1); // id 1 is arbitrary...
+    info->cigar = cigar;
+    
+    int no_refs = info->search_info->records->sequences->used;
+    for (int i = 0; i < no_refs; ++i) {
+        struct fasta_records *records = info->search_info->records;
+        info->ref_name = records->names->strings[i];
+        info->search_info->match_func(records->sequences->strings[i],
+                                      records->seq_sizes->sizes[i],
+                                      pattern, strlen(pattern),
+                                      match_callback, info);
+    }
 }
 
-static void match_callback(int label, size_t index, void * data)
-{
-    struct read_search_info *info = (struct read_search_info*)data;
-    size_t pattern_len = strlen(info->patterns->strings[label]); // FIXME: precompute
-    size_t start_index = index - pattern_len + 1 + 1; // +1 for arithmetic, +1 for 1-indexed
-    sam_line(info->sam_file,
-             info->read_name, info->ref_name, start_index,
-             info->cigars->strings[label],
-             info->read,
-             info->quality);
-}
 
 static void read_callback(const char *read_name,
                           const char *read,
@@ -106,32 +118,26 @@ static void read_callback(const char *read_name,
     // be able to save some time by not doing this, but compared to
     // building and removeing the trie, I don't think it will be much.
     struct read_search_info *info = empty_read_search_info();
-    info->sam_file = search_info->sam_file;
+    info->search_info = search_info;
     info->read = read;
     info->quality = quality;
-    
-    generate_all_neighbours(read, "ACGT", search_info->edit_dist, build_trie_callback, info);
-    compute_failure_links(info->patterns_trie);
-    
     info->read_name = read_name;
-    for (int i = 0; i < search_info->records->names->used; ++i) {
-        info->ref_name = search_info->records->names->strings[i];
-        const char *ref = search_info->records->sequences->strings[i];
-        size_t n = search_info->records->seq_sizes->sizes[i];
-        aho_corasick_match(ref, n, info->patterns_trie, match_callback, info);
-    }
     
+    generate_all_neighbours(read, "ACGT",
+                            search_info->edit_dist,
+                            pattern_callback, info);
     delete_read_search_info(info);
 }
 
 int main(int argc, char * argv[])
 {
     const char *prog_name = argv[0];
-    
+    const char *algorithm = "naive";
     int opt; int edit_dist = 0;
     static struct option longopts[] = {
         { "help",       no_argument,            NULL,           'h' },
         { "distance",   required_argument,      NULL,           'd' },
+        { "algorithm",  required_argument,      NULL,           'a' },
         { NULL,         0,                      NULL,            0  }
     };
     while ((opt = getopt_long(argc, argv, "hd:", longopts, NULL)) != -1) {
@@ -141,13 +147,22 @@ int main(int argc, char * argv[])
                 printf("Options:\n");
                 printf("\t-h | --help:\t\t Show this message.\n");
                 printf("\t-d | --distance:\t Maximum edit distance for the search.\n");
+                printf("\t-a | --algorithm:\t Algorithm to use for the search.\n");
+                printf("\t\t\t\t Choices are:\n");
+                printf("\t\t\t\t\t\"naive\"\n");
+                printf("\t\t\t\t\t\"bmh\" (Boyer-Moore-Horspool)\n");
+                printf("\t\t\t\t\t\"kmp\" (Knuth-Morris-Pratt)\n");
                 printf("\n\n");
                 return EXIT_SUCCESS;
                 
             case 'd':
                 edit_dist = atoi(optarg);
                 break;
-
+                
+            case 'a':
+                algorithm = optarg;
+                break;
+                
             default:
                 fprintf(stderr, "Usage: %s [options] ref.fa reads.fq\n", prog_name);
                 return EXIT_FAILURE;
@@ -175,6 +190,18 @@ int main(int argc, char * argv[])
     
     struct search_info *search_info = empty_search_info();
     search_info->edit_dist = edit_dist;
+    
+    if (strcmp(algorithm, "naive") == 0) {
+        search_info->match_func = naive_exact_match;
+    } else if (strcmp(algorithm, "bmh") == 0) {
+        search_info->match_func = boyer_moore_horspool;
+    } else if (strcmp(algorithm, "bmh") == 0) {
+        search_info->match_func = knuth_morris_pratt;
+    } else {
+        fprintf(stderr, "Unknown search algorithm %s.\n", algorithm);
+        return EXIT_FAILURE;
+    }
+    
     read_fasta_records(search_info->records, fasta_file);
     fclose(fasta_file);
     
