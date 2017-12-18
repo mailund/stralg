680d683d7f4458d49090a4d78471794fc8508d4d
Thomas Mailund <mailund@birc.au.dk>
2017-12-18 15:32:45 +0100

    Removed a SAM column that isn't mandatory.

--- stralg/sam.c
+++ stralg/sam.c
@@ -4,6 +4,6 @@
 void sam_line(FILE *file, const char *qname, const char *rname,
               size_t pos, const char *cigar, const char *seq, const char *qual)
 {
-    fprintf(file, "%s\t0\t%s\t%zu\t0\t%s\t*\t0\t0\t%s\t%s\t*\n",
+    fprintf(file, "%s\t0\t%s\t%zu\t0\t%s\t*\t0\t0\t%s\t%s\n",
             qname, rname, pos, cigar, seq, qual);
 }
