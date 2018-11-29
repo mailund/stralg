474d8e333f8d7ce0c73fdd499d996acdbea5847d
Thomas Mailund <mailund@birc.au.dk>
2018-11-29 16:33:10 +0100

    got rid of old code

--- stralg/fasta.h
+++ stralg/fasta.h
@@ -2,21 +2,8 @@
 #ifndef FASTA_H
 #define FASTA_H
 
-#include <string_vector.h>
-#include <size_vector.h>
 #include <stdio.h>
 #include <stdbool.h>
-
-struct fasta_records {
-    struct string_vector *names;
-    struct string_vector *sequences;
-    struct size_vector *seq_sizes;
-};
-
-struct fasta_records *empty_fasta_records();
-void delete_fasta_records(struct fasta_records *records);
-
-int read_fasta_records(struct fasta_records *records, FILE *file);
 
 // opaque structures.
 struct fasta_file;
