8674ff552ec60928b854f5ecab066b7b1f23b074
Thomas Mailund <mailund@birc.au.dk>
2018-11-30 19:36:40 +0100

    Fixed tests to they work from Xcode as well.

--- tests/run-match-tests.sh
+++ tests/run-match-tests.sh
@@ -1,10 +1,15 @@
 
-#!/bin/sh
+#!/bin/bash
 
-./match_test naive test-data/modest-proposal.txt > test-data/naive-match.txt
-./match_test border test-data/modest-proposal.txt > test-data/border-match.txt
-./match_test kmp test-data/modest-proposal.txt > test-data/kmp-match.txt
-./match_test bmh test-data/modest-proposal.txt > test-data/bmh-match.txt
+export PATH="$PATH:."
+
+./match_test
+match_testd
+
+match_test naive test-data/mest-proposal.txt > test-data/naive-match.txt     || exit 1
+match_test border test-data/modest-proposal.txt > test-data/border-match.txt || exit 1
+match_test kmp test-data/modest-proposal.txt > test-data/kmp-match.txt       || exit 1
+match_test bmh test-data/modest-proposal.txt > test-data/bmh-match.txt       || exit 1
 
 if cmp test-data/naive-match.txt test-data/border-match.txt;
 then
