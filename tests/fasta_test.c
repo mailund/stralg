b1391144ca400bae0497ffc22a0b40b9c805d3e3
Thomas Mailund <mailund@birc.au.dk>
2018-12-01 12:47:50 +0100

    Improved fast tests.

--- tests/fasta_test.c
+++ tests/fasta_test.c
@@ -7,14 +7,15 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
+    if (argc != 3) {
         // LCOV_EXCL_START
         printf("need one argument: fasta file\n");
         return EXIT_FAILURE;
         // LCOV_EXCL_STOP
     }
+    
+    // A test where everything should work out with no errors.
     const char *fname = argv[1];
-
     enum fasta_errors err;
     struct fasta_records *fasta_file = load_fasta_records(fname, &err);
     if (!fasta_file) {
@@ -23,6 +24,7 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
         // LCOV_EXCL_START
     }
+    assert(err == NO_FASTA_ERRORS);
 
     struct fasta_iter iter;
     init_fasta_iter(&iter, fasta_file);
@@ -33,8 +35,25 @@ int main(int argc, char **argv)
         printf("seq len %lu\n", rec.seq_len);
     }
     dealloc_fasta_iter(&iter);
-
     free_fasta_records(fasta_file);
+    
+    // Check that it also works when we use a null err.
+    fasta_file = load_fasta_records(fname, 0);
+    free_fasta_records(fasta_file);
+    
+    // Done with working test.
+    
+    
+    // Test when the file does not exist.
+    assert(!load_fasta_records("no such file", &err));
+    assert(err == CANNOT_OPEN_FASTA_FILE);
+    
+    // Test we capture malformed files
+    const char *malformed = argv[2];
+    assert(!load_fasta_records(malformed, &err));
+    assert(err == MALFORMED_FASTA_RECORD_ERROR);
+    
+
 
     return EXIT_SUCCESS;
 }
