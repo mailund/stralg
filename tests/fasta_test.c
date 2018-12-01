365587943ffd9f1769c5a88002077f5eaca57182
Thomas Mailund <mailund@birc.au.dk>
2018-12-01 11:54:13 +0100

    Excluded lines that shouldn't be run in a test if the test works.

--- tests/fasta_test.c
+++ tests/fasta_test.c
@@ -8,16 +8,20 @@
 int main(int argc, char **argv)
 {
     if (argc != 2) {
+        // LCOV_EXCL_START
         printf("need one argument: fasta file\n");
         return EXIT_FAILURE;
+        // LCOV_EXCL_STOP
     }
     const char *fname = argv[1];
 
     enum fasta_errors err;
     struct fasta_records *fasta_file = load_fasta_records(fname, &err);
     if (!fasta_file) {
+        // LCOV_EXCL_START
         printf("Could not load %s (%d)\n", fname, err);
         return EXIT_FAILURE;
+        // LCOV_EXCL_START
     }
 
     struct fasta_iter iter;
