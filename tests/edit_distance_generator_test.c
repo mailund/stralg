7699ac3b3b909e0f8e8e0eeec076efbeb98fb6e9
Thomas Mailund <mailund@birc.au.dk>
2018-12-01 11:51:37 +0100

    Removed callback from edit distance test. With the iterator interface there is no need for it (and the old interface is gone anyway)

--- tests/edit_distance_generator_test.c
+++ tests/edit_distance_generator_test.c
@@ -6,11 +6,6 @@
 #include <assert.h>
 #include <stdio.h>
 
-void callback(const char *string, const char *cigar, void *data)
-{
-    printf("%s %s\n", string, cigar);
-}
-
 int main(int argc, char * argv[])
 {
     const char *pattern = "acca";
