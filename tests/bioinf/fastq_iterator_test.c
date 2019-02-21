7214bea7c1d9e4d25c4ab35c7f5660e3159737cc
Thomas Mailund <mailund@birc.au.dk>
2019-02-21 18:42:40 +0100

    Updated fastq test
    Resolves #91

--- tests/bioinf/fastq_iterator_test.c
+++ tests/bioinf/fastq_iterator_test.c
@@ -6,17 +6,19 @@
 int main(int argc, char *argv[])
 {
     FILE *input = fopen("test-data/test.fq", "r");
+    FILE *output = fopen("test-data/fastq_iterator_test_current.txt", "w");
     struct fastq_iter iter;
     struct fastq_record record;
     init_fastq_iter(&iter, input);
     while (next_fastq_record(&iter, &record)) {
-        printf("@%s\n", record.name);
-        printf("%s\n", record.sequence);
-        printf("+\n");
-        printf("%s\n", record.quality);
+        fprintf(output, "@%s\n", record.name);
+        fprintf(output, "%s\n", record.sequence);
+        fprintf(output, "+\n");
+        fprintf(output, "%s\n", record.quality);
     }
     dealloc_fastq_iter(&iter);
     fclose(input);
+    fclose(output);
 
     return EXIT_SUCCESS;
 }
