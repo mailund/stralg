744ca00b0e37542bb7bb3d04cfc29d074ce9c26c
Thomas Mailund <mailund@birc.au.dk>
2019-04-10 10:13:49 +0200

    Fixed SAM output

--- bioinf/sam.c
+++ bioinf/sam.c
@@ -5,7 +5,7 @@
 void print_sam_line(FILE *file, const char *qname, const char *rname,
                     uint32_t pos, const char *cigar, const char *seq, const char *qual)
 {
-    fprintf(file, "%s\t0%ss\t%ut0\t%s\t*\t0\t0\t%s\t%s\n",
+    fprintf(file, "%s\t0\t%s\t%u\t0\t%s\t*\t0\t0\t%s\t%s\n",
             qname, rname, pos, cigar, seq, qual);
 }
 
