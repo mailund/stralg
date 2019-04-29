0a2ff3766c729879753d980be86e338c7fe5b5b4
Thomas Mailund <mailund@birc.au.dk>
2019-04-29 13:03:39 +0200

    This is a working version without preprocessing...
    or at least more working; I haven't testing it throughly

--- tools/readmappers/bwt_readmapper/bwt_readmapper.c
+++ tools/readmappers/bwt_readmapper/bwt_readmapper.c
@@ -11,9 +11,6 @@
 #include <stdbool.h>
 #include <getopt.h>
 
-// For debugging. Testing if it works without serialisation
-#define NO_PREPROCESSING 1
-
 static const char *suffix = "bwttables";
 
 static void preprocess(const char *fasta_fname)
@@ -87,7 +84,6 @@ static struct table_list *prepend_table(char *seq_name,
 }
 
 
-#if NO_PREPROCESSING
 struct table_list *build_tables(const char *fasta_fname)
 {
     enum error_codes err;
@@ -119,40 +115,10 @@ struct table_list *build_tables(const char *fasta_fname)
     }
     dealloc_fasta_iter(&iter);
     
-#if 1
-    fprintf(stderr, "THE TABLES:\n");
-    init_fasta_iter(&iter, fasta_records);
-    while (next_fasta_record(&iter, &rec)) {
-        fprintf(stderr, "rec: %s %s %u\n", rec.name, rec.seq, rec.seq_len);
-    }
-    fprintf(stderr, "\n");
-    dealloc_fasta_iter(&iter);
-    /*
-     struct table_list {
-     struct table_list *next;
-     char *seq_name;
-     struct bwt_table *table;
-     };
-     */
-    
-    struct table_list *lst = tables;
-    while (lst) {
-        fprintf(stderr, "mapped table with name '%s'\n", lst->seq_name);
-        char backmap[strlen(lst->table->sa->string) + 1];
-        rev_remap(backmap, lst->table->sa->string, lst->table->remap_table);
-        fprintf(stderr, "the string is '%s'\n", backmap);
-        
-        lst = lst->next;
-    }
-    fprintf(stderr, "\n");
-
-#endif
     free_fasta_records(fasta_records);
 
     return tables;
 }
-
-#else
 
 static struct table_list *read_tables(const char *fasta_fname)
 {
@@ -178,7 +144,6 @@ static struct table_list *read_tables(const char *fasta_fname)
     return tables;
 }
 
-#endif
 
 
 static void delete_tables(struct table_list *tables)
@@ -210,75 +175,11 @@ static void map_records(FILE *outfile,
                         struct fastq_record *fastq_rec,
                         int edits)
 {
-    //char rev_remap_buf[1000];
-    char remap_buf[1000];
-    
-    remap(remap_buf, fastq_rec->sequence, records->table->remap_table);
-    
-    fprintf(stderr, "searching for read '%s' -> ", fastq_rec->sequence);
-    print_remapped(stderr, remap_buf, records->table->remap_table);
-    fprintf(stderr, "\n");
-    
-   
     while (records) {
-#if 1 // for debugging
-#warning doing exact matching
-        //remap(remap_buf, records->table->sa->string, records->table->remap_table);
-        //fprintf(stderr, "remapped : %s\n", remap_buf);
 
-        
-        const char alphabet[] = {1,2,3,4};
-        
-        struct edit_iter iter;
-        struct edit_pattern edit_pattern;
-
+        char remap_buf[1000];
         remap(remap_buf, fastq_rec->sequence, records->table->remap_table);
 
-        init_edit_iter(&iter, remap_buf, alphabet, edits);
-        while (next_edit_pattern(&iter, &edit_pattern)) {
-            // Skip matches with flanking deletions.
-            int dummy; char dummy_str[1000];
-            if (sscanf(edit_pattern.cigar, "%dD%s", &dummy, dummy_str) > 1) {
-                continue;
-            }
-            if (sscanf(edit_pattern.cigar, "%s%dD", dummy_str, &dummy) > 1) {
-                continue;
-            }
-            
-            /*
-            fprintf(stderr, "remapped pattern: ");
-            print_remapped(stderr, edit_pattern.pattern, records->table->remap_table);
-            fprintf(stderr, "\n");
-            */
-            
-            struct bwt_exact_match_iter match_iter;
-            struct bwt_exact_match match;
-            init_bwt_exact_match_iter(&match_iter, records->table, remap_buf);
-            while (next_bwt_exact_match_iter(&match_iter, &match)) {
-                fprintf(stderr, "match at %d: ", match.pos + 1);
-                print_remapped(stderr, remap_buf + 1, records->table->remap_table);
-                fprintf(stderr, " agains ");
-                print_remapped(stderr, edit_pattern.pattern, records->table->remap_table);
-                fprintf(stderr, "\n");
-#if 0
-                print_sam_line(outfile,
-                               fastq_rec->name,
-                               records->seq_name,
-                               match.pos + 1,
-                               edit_pattern.cigar,
-                               fastq_rec->sequence,
-                               fastq_rec->quality);
-#endif
-
-            }
-            dealloc_bwt_exact_match_iter(&match_iter);
-            
-            
-        }
-        records = records->next;
-
-        
-#else
         struct bwt_approx_iter match_iter;
         struct bwt_approx_match match;
         init_bwt_approx_iter(&match_iter, records->table, remapped_seq, edits);
@@ -291,18 +192,266 @@ static void map_records(FILE *outfile,
         }
         dealloc_bwt_approx_iter(&match_iter);
         records = records->next;
-#endif
     }
 }
 
+#if 0
 static void map_read(FILE *outfile,
                      struct fastq_record *fastq_rec,
                      struct table_list *records,
                      int edits)
 {
-    char remapped_read[strlen(fastq_rec->sequence) + 1];
-    remap(remapped_read, fastq_rec->sequence, records->table->remap_table);
-    map_records(outfile, records, remapped_read, fastq_rec, edits);
+    while (records) {
+        
+        char remap_buf[1000];
+        remap(remap_buf, fastq_rec->sequence, records->table->remap_table);
+        
+        struct bwt_approx_iter match_iter;
+        struct bwt_approx_match match;
+        init_bwt_approx_iter(&match_iter, records->table,
+                             remap_buf, edits);
+        while (next_bwt_approx_match(&match_iter, &match)) {
+            print_sam_line(outfile, fastq_rec->name,
+                           records->seq_name,
+                           match.position + 1,
+                           match.cigar, fastq_rec->sequence,
+                           fastq_rec->quality);
+        }
+        dealloc_bwt_approx_iter(&match_iter);
+        records = records->next;
+    }
+    
+}
+#endif
+
+#if BORDER
+void map_read(struct fastq_record *fastq_rec,
+              struct fasta_records *fasta_records,
+              int d,
+              FILE *samfile)
+{
+    struct edit_iter iter;
+    struct edit_pattern edit_pattern;
+    
+    init_edit_iter(&iter, fastq_rec->sequence, "acgt", d);
+    while (next_edit_pattern(&iter, &edit_pattern)) {
+      
+        int dummy; char dummy_str[1000];
+        if (sscanf(edit_pattern.cigar, "%dD%s", &dummy, dummy_str) > 1) {
+            continue;
+        }
+        if (sscanf(edit_pattern.cigar, "%s%dD", dummy_str, &dummy) > 1) {
+            continue;
+        }
+
+        
+        struct fasta_iter fasta_iter;
+        struct fasta_record fasta_rec;
+        
+        init_fasta_iter(&fasta_iter, fasta_records);
+        while (next_fasta_record(&fasta_iter, &fasta_rec)) {
+            
+            struct border_match_iter match_iter;
+            struct match match;
+            
+            init_border_match_iter(&match_iter, fasta_rec.seq, fasta_rec.seq_len, edit_pattern.pattern, strlen(edit_pattern.pattern));
+            
+            while (next_border_match(&match_iter, &match)) {
+                print_sam_line(samfile, fastq_rec->name, fasta_rec.name, match.pos + 1, edit_pattern.cigar, fastq_rec->sequence, fastq_rec->quality);
+            }
+            
+            dealloc_border_match_iter(&match_iter);
+            
+            
+        }
+        dealloc_fasta_iter(&fasta_iter);
+        
+    }
+    dealloc_edit_iter(&iter);
+}
+#endif
+
+struct string_table {
+    const char *name;
+    struct bwt_table *bwt_table;
+    struct string_table *next;
+};
+
+static struct string_table *new_string_table(const char *name,
+                                             struct bwt_table *bwt_table,
+                                             struct string_table *next)
+{
+    struct string_table *res = malloc(sizeof(struct string_table));
+    res->name = name;
+    res->bwt_table = bwt_table;
+    res->next = next;
+    return res;
+}
+
+static struct string_table *build_string_tables(struct fasta_records *fasta_records)
+{
+    struct string_table *tables = 0;
+    
+    struct fasta_iter iter;
+    struct fasta_record rec;
+    
+    init_fasta_iter(&iter, fasta_records);
+    while (next_fasta_record(&iter, &rec)) {
+        const char *name = rec.name;
+        
+        struct remap_table *remap_table = alloc_remap_table(rec.seq);
+        
+        char *remapped_seq = malloc(rec.seq_len + 1);
+        remap(remapped_seq, rec.seq, remap_table);
+        struct suffix_array *sa = qsort_sa_construction(remapped_seq);
+        
+        struct bwt_table *bwt_table = alloc_bwt_table(sa, remap_table);
+        
+        tables = new_string_table(name, bwt_table, tables);
+    }
+    dealloc_fasta_iter(&iter);
+    
+    return tables;
+}
+
+static void free_string_tables(struct string_table *tables)
+{
+    struct string_table *next;
+    while (tables) {
+        next = tables->next;
+        free_complete_bwt_table(tables->bwt_table);
+        free(tables);
+        tables = next;
+    }
+}
+
+#if 0 // another checkpoint
+void map_read(struct fastq_record *fastq_rec,
+              struct string_table *records,
+              int d,
+              FILE *samfile)
+{
+    struct edit_iter iter;
+    struct edit_pattern edit_pattern;
+    char remap_buf[1000];
+    
+    while (records) {
+        init_edit_iter(&iter, fastq_rec->sequence, "acgt", d);
+        while (next_edit_pattern(&iter, &edit_pattern)) {
+            
+            remap(remap_buf, edit_pattern.pattern, records->bwt_table->remap_table);
+            
+            int dummy; char dummy_str[1000];
+            if (sscanf(edit_pattern.cigar, "%dD%s", &dummy, dummy_str) > 1) {
+                continue;
+            }
+            if (sscanf(edit_pattern.cigar, "%s%dD", dummy_str, &dummy) > 1) {
+                continue;
+            }
+            
+            struct sa_match_iter sa_iter;
+            struct sa_match sa_match;
+            init_sa_match_iter(&sa_iter, remap_buf, records->bwt_table->sa);
+            while (next_sa_match(&sa_iter, &sa_match)) {
+              print_sam_line(samfile, fastq_rec->name, records->name,
+                             sa_match.position + 1, edit_pattern.cigar, fastq_rec->sequence, fastq_rec->quality);
+            }
+            
+            dealloc_sa_match_iter(&sa_iter);
+
+            /*print_sam_line(samfile, fastq_rec->name, fasta_rec.name, match.pos + 1, edit_pattern.cigar, fastq_rec->sequence, fastq_rec->quality);*/
+        }
+        dealloc_edit_iter(&iter);
+
+        
+        records = records->next;
+    }
+    
+}
+#endif
+
+
+#if 0 // now we are getting there...
+void map_read(struct fastq_record *fastq_rec,
+              struct string_table *records,
+              int d,
+              FILE *samfile)
+{
+    struct edit_iter iter;
+    struct edit_pattern edit_pattern;
+    char remap_buf[1000];
+    
+    while (records) {
+        init_edit_iter(&iter, fastq_rec->sequence, "acgt", d);
+        while (next_edit_pattern(&iter, &edit_pattern)) {
+            
+            remap(remap_buf, edit_pattern.pattern, records->bwt_table->remap_table);
+            
+            int dummy; char dummy_str[1000];
+            if (sscanf(edit_pattern.cigar, "%dD%s", &dummy, dummy_str) > 1) {
+                continue;
+            }
+            if (sscanf(edit_pattern.cigar, "%s%dD", dummy_str, &dummy) > 1) {
+                continue;
+            }
+            
+#if 0
+            struct sa_match_iter sa_iter;
+            struct sa_match sa_match;
+            init_sa_match_iter(&sa_iter, remap_buf, records->bwt_table->sa);
+            while (next_sa_match(&sa_iter, &sa_match)) {
+                print_sam_line(samfile, fastq_rec->name, records->name,
+                               sa_match.position + 1, edit_pattern.cigar, fastq_rec->sequence, fastq_rec->quality);
+            }
+            
+            dealloc_sa_match_iter(&sa_iter);
+#endif
+
+            struct bwt_exact_match_iter iter;
+            struct bwt_exact_match match;
+            
+            init_bwt_exact_match_iter(&iter, records->bwt_table, remap_buf);
+            while (next_bwt_exact_match_iter(&iter, &match)) {
+                print_sam_line(samfile, fastq_rec->name, records->name, match.pos + 1, edit_pattern.cigar, fastq_rec->sequence, fastq_rec->quality);
+            }
+            dealloc_bwt_exact_match_iter(&iter);
+            
+        }
+        dealloc_edit_iter(&iter);
+        
+        records = records->next;
+    }
+    
+}
+#endif
+
+void map_read(struct fastq_record *fastq_rec,
+              struct string_table *records,
+              int d,
+              FILE *samfile)
+{
+    char remap_buf[1000];
+    
+    while (records) {
+        remap(remap_buf, fastq_rec->sequence, records->bwt_table->remap_table);
+        
+        struct bwt_approx_iter  iter;
+        struct bwt_approx_match match;
+        
+        init_bwt_approx_iter(&iter, records->bwt_table, remap_buf, d);
+        while (next_bwt_approx_match(&iter, &match)) {
+            print_sam_line(samfile,
+                           fastq_rec->name, records->name,
+                           match.position + 1,
+                           match.cigar,
+                           fastq_rec->sequence, fastq_rec->quality);
+
+        }
+        dealloc_bwt_approx_iter(&iter);
+            
+        records = records->next;
+    }
+    
 }
 
 static void print_help(const char *progname)
@@ -376,8 +525,53 @@ int main(int argc, char **argv)
         fasta_fname = argv[0];
         fastq_fname = argv[1];
         
+        
+        // CURRENT ATTEMPT
+        enum error_codes err;
+        struct fasta_records *fasta_records = load_fasta_records(fasta_fname, &err);
+        switch (err) {
+            case NO_ERROR:
+                break;
+                
+            case CANNOT_OPEN_FILE:
+                printf("Cannot open fasta file: %s\n", fasta_fname);
+                perror("Error");
+                exit(EXIT_FAILURE);
+                
+            case MALFORMED_FILE:
+                printf("The fasta file is malformed: %s\n", fasta_fname);
+                exit(EXIT_FAILURE);
+                
+            default:
+                assert(false); // this is not an error the function should return
+        }
+        
+        struct string_table *tables = build_string_tables(fasta_records);
+        
+        FILE *samfile = stdout; // FIXME: option for writing to a file?
         FILE *fastq_file = fopen(fastq_fname, "r");
-#if NO_PREPROCESSING
+
+        struct fastq_iter fastq_iter;
+        struct fastq_record fastq_rec;
+        init_fastq_iter(&fastq_iter, fastq_file);
+        while (next_fastq_record(&fastq_iter, &fastq_rec)) {
+            void map_read(struct fastq_record *fastq_rec,
+                          struct string_table *records,
+                          int d,
+                          FILE *samfile);
+
+            map_read(&fastq_rec, tables, edits, samfile);
+        }
+        dealloc_fastq_iter(&fastq_iter);
+        free_string_tables(tables);
+        free_fasta_records(fasta_records);
+        // END CURRENT ATTEMPT
+        
+#if 0
+        
+        FILE *fastq_file = fopen(fastq_fname, "r");
+#if 1
+#warning "building tables instead of reading them"
         struct table_list *tables = build_tables(fasta_fname);
 #else
         struct table_list *tables = read_tables(fasta_fname);
@@ -386,10 +580,56 @@ int main(int argc, char **argv)
             printf("No FASTA records found in file %s\n", fasta_fname);
             return EXIT_FAILURE;
         }
+
         
         FILE *samfile = stdout; // FIXME: option for writing to a file?
+        fastq_file = fopen(fastq_fname, "r");
+        struct fastq_iter fastq_iter;
+        struct fastq_record fastq_rec;
+        init_fastq_iter(&fastq_iter, fastq_file);
+        while (next_fastq_record(&fastq_iter, &fastq_rec)) {
+            map_read(samfile, &fastq_rec, tables, edits);
+        }
+        dealloc_fastq_iter(&fastq_iter);
+        
+#endif
+        
+#if 0
+        enum error_codes err;
+        struct fasta_records *fasta_records =           load_fasta_records(fasta_fname, &err);
+        
+        switch (err) {
+            case NO_ERROR:
+                break;
+                
+            case CANNOT_OPEN_FILE:
+                printf("Cannot open fasta file: %s\n", fasta_fname);
+                return EXIT_FAILURE;
+                
+            case MALFORMED_FILE:
+                printf("The fasta file is malformed: %s\n", fasta_fname);
+                return EXIT_FAILURE;
+                
+            default:
+                assert(false); // this is not an error the function should return
+        }
+        
+        FILE *samfile = stdout; // FIXME: option for writing to a file?
+        fastq_file = fopen(fastq_fname, "r");
+        struct fastq_iter fastq_iter;
+        struct fastq_record fastq_rec;
+        init_fastq_iter(&fastq_iter, fastq_file);
+        while (next_fastq_record(&fastq_iter, &fastq_rec)) {
+            map_read(&fastq_rec, fasta_records, edits, samfile);
+        }
+        dealloc_fastq_iter(&fastq_iter);
+        
+        free_fasta_records(fasta_records);
+
+#endif
         
         
+#if 0
         struct fastq_iter reads_iter;
         struct fastq_record fastq_rec;
         init_fastq_iter(&reads_iter, fastq_file);
@@ -401,8 +641,10 @@ int main(int argc, char **argv)
         if (samfile != stdout) fclose(samfile);
         fclose(fastq_file);
         delete_tables(tables);
+#endif
     }
     
     
     return EXIT_SUCCESS;
 }
+
