b21009775071ebbaa9a261e1b9e42973cb063060
Thomas Mailund <mailund@birc.au.dk>
2019-06-24 12:47:02 +0200

    fixed bug (needed inverse SA. D'uh)

--- tests/stralg/skew_test.c
+++ tests/stralg/skew_test.c
@@ -8,12 +8,47 @@
 #include <stdbool.h>
 
 
-
-static void set_12_3_sizes(size_t n, size_t *m12, size_t *m3)
+static bool uint16_less(uint16_t *x, size_t i, size_t n, uint16_t *y, size_t j, size_t m)
 {
-    *m3 = (n - 1) / 3 + 1; // n - 1 to adjust for 0 indexing and + 1 to pick zero
-    *m12 = n - *m3;
+    while (i < n && j < m) {
+        if (x[i] < y[j]) return true;
+        if (x[i] > y[j]) return false;
+        i++; j++;
+    }
+    if (i == n) return true;
+    if (j == m) return false;
+    assert(false);
+    
+    return true;
 }
+
+static bool check_sa(size_t *sa, size_t m, uint16_t *s, size_t n)
+{
+    for (size_t i = 1; i < m; ++i) {
+        if (!uint16_less(s, sa[i - 1], n, s, sa[i], n))
+            return false;
+    }
+    return true;
+}
+
+static void print_uint16_string(size_t *sa, size_t i, uint16_t *s, size_t n)
+{
+    printf("sa[%2lu] == %2lu\t:\t", i, sa[i]);
+    for (size_t j = sa[i]; j < n; ++j) {
+        printf("%u.", s[j]);
+    }
+    printf("\n");
+}
+
+static void print_sa(const char *header, size_t *sa, size_t m, uint16_t *s, size_t n)
+{
+    printf("%s\n", header);
+    for (size_t i = 0; i < m; ++i) {
+        print_uint16_string(sa, i, s, n);
+    }
+    printf("\n");
+}
+
 
 // Map from indices in s to indices in s12
 static size_t map_s_s12(size_t k) {
@@ -26,24 +61,10 @@ static size_t map_s12_s(size_t k)
     return k + k / 2 + 1;
 }
 
-// map from an index in s3 to an index in s
-static size_t map_s_s3(size_t k)
-{
-    return k / 3;
-}
-
-
-// map from an index in u to an index in s12
-static size_t map_u_s12(size_t i, size_t m)
-{
-    assert(i != m); // don't touch sentinel
-    return (i < m) ? (2 * i + 1) : (2 * (i - m - 1));
-}
-
 // map from an index in u to an index in s
 static size_t map_u_s(size_t i, size_t m)
 {
-    size_t k = map_u_s12(i, m);
+    size_t k = (i < m) ? (2 * i + 1) : (2 * (i - m - 1));
     return map_s12_s(k);
 }
 
@@ -117,11 +138,8 @@ static bool equal3(uint16_t *s, size_t n, size_t i, size_t j)
 
 
 static uint16_t lex3sort(uint16_t *s, size_t n, uint16_t alphabet_size,
-                         size_t *sa12, uint16_t *s12_lex3_numbers)
+                         size_t *sa12, size_t m12, uint16_t *s12_lex3_numbers)
 {
-    size_t m12, m3;
-    set_12_3_sizes(n, &m12, &m3);
-
     // set up s12 and sort s12
     for (size_t i = 0, j = 0; i < n; ++i) {
         if (i % 3 != 0) {
@@ -193,7 +211,7 @@ static void construct_sa3(size_t m12, size_t m3, size_t n, uint16_t *s, size_t *
     radix_sort_1(s, n, sa3, m3, 256);
 }
 
-static bool less(size_t i, size_t j, uint16_t *s, size_t *sa12, size_t *sa3, size_t n)
+static bool less(size_t i, size_t j, uint16_t *s, size_t n, size_t *isa)
 {
     // Since we do not have the terminal sentinel
     // in this algorithm we need to test the indices
@@ -207,30 +225,44 @@ static bool less(size_t i, size_t j, uint16_t *s, size_t *sa12, size_t *sa3, siz
     
     // Check cases where we have the indices in the
     // same arrays
-    if ((i % 3 == 0) && (j % 3 == 0)) {
-        return sa3[map_s_s3(i)] < sa3[map_s_s3(j)];
+    if (((i % 3 == 0) && (j % 3 == 0))||((i % 3 != 0) && (j % 3 != 0))) {
+        return isa[i] < isa[j];
     }
-    if ((i % 3 != 0) && (j % 3 != 0)) {
-        return sa12[map_s_s12(i)] < sa12[map_s_s12(j)];
-    }
-    
+
     // Recurse otherwise; they will end up in the same
     // arrays after max two recursions
-    return less(i + 1, j + 1, s, sa12, sa3, n);
+    return less(i + 1, j + 1, s, n, isa);
 }
 
-static void merge_suffix_arrays(uint16_t *s, size_t *sa12, size_t m12, size_t *sa3, size_t m3, size_t *sa)
+static void merge_suffix_arrays(uint16_t *s,
+                                size_t *sa12, size_t m12,
+                                size_t *sa3, size_t m3,
+                                size_t *sa)
 {
     size_t i = 0, j = 0, k = 0;
     size_t n = m12 + m3;
     
+    // FIXME: don't stack allocate
+    size_t isa[n];
+
+    // we are essentially building sa[i] (although
+    // not sorting between 12 and 3, and then doing
+    // isa[sa[i]] = i. Just both at the same time.
+    for (size_t i = 1, j = 0; j < m12; i += 3, j += 2) {
+        isa[sa12[j]] = i;
+    }
+    for (size_t i = 2, j = 1; j < m12; i += 3, j += 2) {
+        isa[sa12[j]] = i;
+    }
+    for (size_t i = 0, j = 0; j < m3; i += 3, j++) {
+        isa[sa3[j]] = i;
+    }
+    
     while (i < m12 && j < m3) {
-        assert(i < m12 + m3);
-        assert(j < m12 + m3);
-        
         size_t ii = sa12[i];
         size_t jj = sa3[j];
-        if (less(ii, jj, s, sa12, sa3, n)) {
+
+        if (less(ii, jj, s, n, isa)) {
             sa[k++] = ii;
             i++;
         } else {
@@ -250,15 +282,15 @@ static void merge_suffix_arrays(uint16_t *s, size_t *sa12, size_t m12, size_t *s
 
 static void skew_rec(uint16_t *s, size_t n, uint16_t alphabet_size, size_t *sa)
 {
-    size_t m12, m3;
-    set_12_3_sizes(n, &m12, &m3);
-    
+    size_t m3 = (n - 1) / 3 + 1; // n - 1 to adjust for 0 indexing and + 1 to pick zero
+    size_t m12 = n - m3;
+
     //uint16_t *lex_nos = malloc(m12 * sizeof(*lex_nos));
     uint16_t lex_nos[m12];
     //size_t *sa12 = malloc(m12 * sizeof(*sa12));
     size_t sa12[m12];
 
-    size_t mapped_alphabet_size = lex3sort(s, n, alphabet_size, sa12, lex_nos); // FIXME: alphabet size
+    size_t mapped_alphabet_size = lex3sort(s, n, alphabet_size, sa12, m12, lex_nos);
     
     // the +1 here is because we leave space for the sentinel
     if (mapped_alphabet_size != m12 + 1) {
@@ -285,13 +317,15 @@ static void skew_rec(uint16_t *s, size_t n, uint16_t alphabet_size, size_t *sa)
     //size_t *sa3 = malloc(m3 * sizeof(*sa3));
     size_t sa3[m3];
     construct_sa3(m12, m3, n, s, sa12, sa3);
-
+    
     merge_suffix_arrays(s, sa12, m12, sa3, m3, sa);
     
     // FIXME free(lex_nos);
     // FIXME free(sa12);
     // FIXME free(sa3);
-
+    
+    //print_sa("After merge", sa, n, s, n);
+    assert(check_sa(sa, n, s, n));
 }
 
 
@@ -308,20 +342,22 @@ static void skew(const char *x, size_t *sa)
         s[i] = x[i];
     }
 
-    skew_rec(s, n, 256, sa + 1);
-    sa[0] = n;
+    skew_rec(s, n, 256, sa + 1); // do not include index zero
+    sa[0] = n; // but set it to the sentinel here
+    
+    print_sa("DONE", sa, n, s, n);
 }
 
 int main(int argc, const char **argv)
 {
-    const char *x = "aaaababaaaaa";
+    const char *x = "aaababaababababaabababbbabaaa";
     size_t n = strlen(x);
     size_t sa[n + 1];
     
     skew(x, sa);
     
     for (size_t i = 0; i < n; ++i) {
-        printf("sa[%.2lu] = %.2lu: %s\n", i, sa[i], x + sa[i]);
+        printf("sa[%2lu] = %2lu: %s\n", i, sa[i], x + sa[i]);
     }
     printf("\n");
 
