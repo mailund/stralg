26fd912c08c4680f5c24fe0650df9f34cb7c549f
Thomas Mailund <mailund@birc.au.dk>
2019-06-27 10:37:56 +0200

    Performance code again.

--- performance/suffix_tree_construction.R
+++ performance/suffix_tree_construction.R
@@ -2,10 +2,13 @@ library(tidyverse)
 
 performance1 <- read_table2("suffix_tree_construction_v1.txt",
                            col_names = c("Algorithm", "String", "Size", "Time"))
-performance2 <- read_table2("suffix_tree_construction.txt",
+performance2 <- read_table2("suffix_tree_construction_v2.txt",
                            col_names = c("Algorithm", "String", "Size", "Time"))
+current_performance <- read_table2("suffix_tree_construction.txt",
+                            col_names = c("Algorithm", "String", "Size", "Time"))
 
-performance <- rbind(performance1, performance2)
+
+performance <- rbind(performance1, performance2, current_performance)
 
 ggplot(performance,
        aes(x = Size, y = Time, color = Algorithm)) +
@@ -26,11 +29,27 @@ comparison_data <- inner_join(
     filter(performance2, Algorithm != "naive"),
     by = c("String", "Size")) %>%
     rename(`First McCreight` = Time.x,
-           `Node pool McCreight` = Time.y) %>%
+           `Mempool McCreight` = Time.y) %>%
     select(-starts_with("Algorithm."))
 
 comparison_data %>%
-    ggplot(aes(x = Size, y = `Node pool McCreight` / `First McCreight`, color = String)) +
+    ggplot(aes(x = Size, y = `Mempool McCreight` / `First McCreight`, color = String)) +
+    geom_jitter() +
+    geom_smooth(method = "lm") +
+    theme_minimal()
+
+comparison_data <- inner_join(
+    performance2,
+    filter(current_performance, Algorithm != "naive"),
+    by = c("String", "Size")) %>%
+    rename(`Mempool McCreight` = Time.x,
+           `Current McCreight` = Time.y) %>%
+    select(-starts_with("Algorithm."))
+
+comparison_data %>%
+    filter(Size > 1000) %>% # too much noise at small sizes
+    ggplot(aes(x = Size, y = `Current McCreight` / `Mempool McCreight`, color = String)) +
+    facet_grid(String ~ .) + #, scales = "free_y") +
     geom_jitter() +
     geom_smooth(method = "lm") +
     theme_minimal()
