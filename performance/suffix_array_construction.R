9ae6e5b394be877f74747e86c17d2a5a0664e5e1
Thomas Mailund <mailund@birc.au.dk>
2019-07-04 12:52:07 +0200

    profiling

--- performance/suffix_array_construction.R
+++ performance/suffix_array_construction.R
@@ -4,9 +4,11 @@ performance6 <- read_table2("suffix_array_construction_v6.txt",
                             col_names = c("Algorithm", "String", "Size", "Time"))
 performance7 <- read_table2("suffix_array_construction_v7.txt",
                             col_names = c("Algorithm", "String", "Size", "Time"))
+performance8 <- read_table2("suffix_array_construction_v8.txt",
+                            col_names = c("Algorithm", "String", "Size", "Time"))
 
 
-performance <- rbind(performance6, performance7)
+performance <- rbind(performance6, performance7, performance8)
 
 ggplot(performance,
        aes(x = Size, y = Time, color = Algorithm)) +
