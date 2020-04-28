f9a169ca97ec37c6899a44fe83a2a6af627110c5
Thomas Mailund <mailund@birc.au.dk>
2020-04-28 09:20:01 +0200

    setup for new full experiments

--- performance/suffix_array_search.sh
+++ performance/suffix_array_search.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-program=../xcodebuild/performance/Debug/suffix_array_search
+program=./suffix_array_search
 
-for (( n = 500; n <= 600; n += 10 ))
+#for (uint32_t n = 10000; n <= 250000; n += 1000) {
+for (( n = 100000; n <= 10000000; n += 100000 ))
 do
     for (( m = 100; m <= 500; m += 100 ))
     do
